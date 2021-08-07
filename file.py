98fd1d8da59fe4ffabf70819704b7892b1c0fa85
Boris Borkovic <bborkovic@gmail.com>
2021-08-07 18:16:15 +0200

    branch PROD cleared!

--- file.py
+++
@@ -1,5 +0,0 @@
-from repo import Repo
-
-class Klasa:
-  def __initialize__:
-    this.a = 100
--- funkcija.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  print("ovo je nova funkcija");
-  print("novi redak u main funkciji / branch ");
-  print("ovo je novi redak u main funkciji");
-
-
-
-?>
